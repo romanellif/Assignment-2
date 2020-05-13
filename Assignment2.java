fcd61359d7ea0eb3e8a33c7015d1304acc7ae216
romanellif <romanellifxox@gmail.com>
2020-05-13 16:41:35 -0400

    Options available

--- Assignment2.java
+++ Assignment2.java
@@ -10,4 +10,13 @@ public class Assignment2 {
 
 	public static void main(String args[]) {
 
+		System.out.println("Choose one of the following options:");
+		System.out.println("1.Make a deposit");
+		System.out.println("2.Withdraw");
+		System.out.println("3.Transfer between accounts");
+		System.out.println("4.Print balance");
+		System.out.println("5.Make a payment");
+
+
 	}
+}
